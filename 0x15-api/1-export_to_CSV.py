374514016c10d23c9ef42a30274e7442b845dea8
cherjiosclear <slopez0286@gmail.com>
2020-02-04 02:45:33 +0000

    Fixing 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,25 +8,22 @@ import sys
 import csv
 
 
-def do_request():
+if __name__ == "__main__":
     r = requests.get('https://jsonplaceholder.typicode.com/users/{}'
                      .format(sys.argv[1]))
     d = r.json()
-    name = d.get('name')
+    name = d.get('username')
     user_id = d.get('id')
     r = requests.get('https://jsonplaceholder.typicode.com/todos')
     d_todos = r.json()
-    completed = []
     titles = []
-    for index in d_todos:
-        if index.get('userId') == user_id:
-            titles.append(index.get('title'))
-            completed.append(index.get('completed'))
+    completed = []
+    for i in d_todos:
+        if i.get('userId') == user_id:
+            titles.append(i.get('title'))
+            completed.append(i.get('completed'))
     with open('{}.csv'.format(sys.argv[1]), mode='w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',', quotechar='"',
                             quoting=csv.QUOTE_ALL)
-        for index, num in enumerate(titles):
-            writer.writerow([user_id, name, completed[index], num])
-
-if __name__ == "__main__":
-    do_request()
+        for i, e in enumerate(titles):
+            writer.writerow([user_id, name, completed[i], e])
